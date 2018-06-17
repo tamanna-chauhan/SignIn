fdf204976852d56ae7e4a0613813e185c23e6832
Tamanna Chauhan <chauhantamanna001@gmail.com>
2018-06-16 23:10:39 -0700

    second commit

--- app/src/main/java/com/acadview/signin/MainActivity.java
+++ app/src/main/java/com/acadview/signin/MainActivity.java
@@ -1,4 +1,4 @@
-package com.acadview.signin;
+package com.acadview.button;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -6,15 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.acadview.signin.Main2Activity;
+import com.acadview.signin.R;
 
-    Button myNewButton;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button myButton = findViewById(R.id.button2);
+        Button myButton = findViewById(R.id.button);
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,6 +25,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+
     }
 }
-
